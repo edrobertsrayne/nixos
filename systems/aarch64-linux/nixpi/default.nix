16512b47b53c50e01a07c267383e903ef5e8300f
Ed Roberts Rayne <ed.rayne@gmail.com>
2024-04-29 18:07:17 +0100

    fix(nixpi): add hardware configuration

--- systems/aarch64-linux/nixpi/default.nix
+++ systems/aarch64-linux/nixpi/default.nix
@@ -10,8 +10,7 @@ with lib;
 with lib.custom; {
   imports = [
     inputs.hardware.nixosModules.raspberry-pi-4
-    (modulesPath + /installer/scan/not-detected.nix)
-    (modulesPath + /installer/sd-card/sd-image-aarch64.nix)
+    ./hardware-configuration.nix
   ];
 
   hardware.networking.enable = mkForce false;
