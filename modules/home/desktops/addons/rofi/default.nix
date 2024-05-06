f97d43e0f629c91aca5b1a19beeef41dc34211ea
Ed Roberts Rayne <ed.rayne@gmail.com>
2024-05-06 22:19:44 +0100

    feat(rofi) add package option and set default to wayland

--- modules/home/desktops/addons/rofi/default.nix
+++ modules/home/desktops/addons/rofi/default.nix
@@ -8,11 +8,25 @@ with lib;
 with lib.custom; let
   cfg = config.custom.desktops.addons.rofi;
 in {
-  options.custom.desktops.addons.rofi.enable = mkEnableOption "Whether to eneable rofi application launcher.";
-
+  options.custom.desktops.addons.rofi = {
+    enable = mkEnableOption "Whether to eneable rofi application launcher.";
+    package = mkPackageOption pkgs "rofi-wayland" {};
+  };
   config = mkIf cfg.enable {
     programs.rofi = {
-      enable = true;
+      inherit (cfg) package enable;
+      font = "Noto 12";
+      extraConfig = {
+        modi = "run,drun,window";
+        show-icons = true;
+        disable-history = false;
+        drun-display-format = "{icon} {name}";
+        hide-scrollbar = true;
+        display-drun = "   Apps ";
+        display-run = "   Run ";
+        display-window = " 﩯  Window";
+        display-Network = " 󰤨  Network";
+      };
     };
   };
 }
