5623479de51590becb52f6fb594adc496de3b477
Ed Roberts Rayne <ed.rayne@gmail.com>
2024-05-07 23:36:31 +0100

    fix(hypridle): move to home-manager configuration

--- modules/home/desktops/addons/hypridle/default.nix
+++ modules/home/desktops/addons/hypridle/default.nix
@@ -9,32 +9,34 @@ with lib;
 with lib.custom; let
   cfg = config.desktops.addons.hypridle;
 in {
-  imports = [inputs.hypridle.homeManagerModules.default];
-
   options.desktops.addons.hypridle.enable = mkEnableOption "hypridle idle daemon";
 
   config = mkIf cfg.enable {
     services.hypridle = {
       enable = true;
-      beforeSleepCmd = "${pkgs.systemd}/bin/loginctl lock-session";
-      lockCmd = lib.getExe config.programs.hyprlock.package;
-      afterSleepCmd = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
-
-      listeners = [
-        {
-          timeout = 600;
-          onTimeout = "${pkgs.systemd}/bin/loginctl lock-session";
-        }
-        {
-          timeout = 660;
-          onTimeout = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
-          onResume = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
-        }
-        {
-          timeout = 1800;
-          onTimeout = "${pkgs.systemd}/bin/systemctl suspend";
-        }
-      ];
+      settings = {
+        general = {
+          before_sleep_cmd = "${pkgs.systemd}/bin/loginctl lock-session";
+          lock_cmd = lib.getExe config.programs.hyprlock.package;
+          after_sleep_cmd = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
+          ignore_dbus_inhibit = false;
+        };
+        listener = [
+          {
+            timeout = 600;
+            on-timeout = "${pkgs.systemd}/bin/loginctl lock-session";
+          }
+          {
+            timeout = 660;
+            on-timeout = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
+            on-resume = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
+          }
+          {
+            timeout = 1800;
+            on-timeout = "${pkgs.systemd}/bin/systemctl suspend";
+          }
+        ];
+      };
     };
   };
 }
