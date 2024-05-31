8057fc7c672c6a4c41c88c3416d5b1274c0bd26c
Ed Roberts Rayne <ed.rayne@gmail.com>
2024-05-31 15:32:51 +0100

    yabai: init

---
+++ modules/darwin/services/yabai/default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  config,
+  ...
+}:
+with lib;
+with lib.custom; let
+  cfg = config.custom.services.yabai;
+in {
+  options.custom.services.yabai.enable = mkEnableOption "Whether to enable yabai window manager";
+
+  config = mkIf cfg.enable {
+    services.yabai = {
+      enable = true;
+      config = {
+        layout = "bsp";
+        window_placement = "second_child";
+        top_padding = 10;
+        bottom_padding = 10;
+        left_padding = 10;
+        right_padding = 10;
+        window_gap = 10;
+        mouse_follows_focus = "on";
+        mouse_modifier = "alt";
+        mouse_action1 = "move";
+        mouse_action2 = "resize";
+        mouse_drop_action = "swap";
+      };
+      extraConfig = ''
+        yabai -m rule --add app="^System Settings$" manage=off
+        yabai -m rule --add app="^Calculator$" manage=off
+      '';
+    };
+  };
+}
