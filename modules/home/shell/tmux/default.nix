0f1ecbff94755b333e139658bc98ccd9a05d3334
Ed Roberts Rayne <ed.rayne@gmail.com>
2024-07-08 21:48:24 +0100

    tmux: switch to stylix for theming and set term to screen-256color

--- modules/home/shell/tmux/default.nix
+++ modules/home/shell/tmux/default.nix
@@ -8,19 +8,23 @@ with lib;
 with lib.custom; let
   cfg = config.custom.shell.tmux;
 in {
-  options.custom.shell.tmux.enable = mkEnableOption "Whether tmux.";
+  options.custom.shell.tmux.enable = mkEnableOption "Whether to enable tmux.";
 
   config = mkIf cfg.enable {
     programs.tmux = {
       enable = true;
       mouse = true;
       shortcut = "s";
+      terminal = "tmux-256color";
       newSession = true;
+      shell = "${pkgs.zsh}/bin/zsh";
+      escapeTime = 0;
       plugins = with pkgs.tmuxPlugins; [
-        catppuccin
         vim-tmux-navigator
       ];
       extraConfig = ''
+        set-option -sa terminal-features ',alacritty:RGB'
+
         bind | split-window -h -c "#{pane_current_path}"
         bind - split-window -v -c "#{pane_current_path}"
         set-option -g status-position top
